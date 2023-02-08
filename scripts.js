7d7742162cc8443b2934edc38f026cd4f7de8db5
mnymkr <hoang.tran.200032@student.fulbright.edu.vn>
2023-02-08 16:39:03 +0700

    Completed the starting chip and editing players logic

--- scripts.js
+++ scripts.js
@@ -1,28 +1,69 @@
-var players = [];
+const starting_chip_section = document.getElementById("starting_chip_section");
+const player_name_row = document.getElementById("player_name_row"); // get new player name
+const player_chips_row = document.getElementById("player_chips_row"); // get game's chip number
+const game_body_section = document.getElementById("game_body_section");
+const new_player_name_input = document.getElementById("new_player_name_input");
+const new_player_name_form = document.getElementById("new_player_name_form");
+const game_body = document.getElementById("game_body");
+const starting_chip_textarea = document.getElementById("starting_chip_textarea");
+const game_control_button = document.getElementById("game_control_button");
+
+var starting_chips
+var players = []; // {name:name, chip:chip}
 var is_in_game = false;
 
+function confirm_starting_chip() {
+    var key = window.event.keyCode;
+
+    if (key === 13) {
+        starting_chip_section.style.display = "none"; // hide the initial screen
+        game_chips = parseInt(starting_chip_textarea.value); // set the game's chip
+        game_body_section.style.display = "block"; // show the game body 
+        new_player_name_input.focus();
+        new_player_name_form.style.display = "block";
+    }
+}
+function confirm_starting_chip_button() {
+    starting_chip_section.style.display = "none"; // hide the initial screen
+    game_chips = parseInt(starting_chip_textarea.value); // set the game's chip
+    game_body_section.style.display = "block"; // show the game body 
+    new_player_name_input.focus();
+    new_player_name_form.style.display = "block";
+}
 function add_player() {
-    player_name_row = document.getElementById("player_name_row"); // get new player name
-    player_chips_row = document.getElementById("player_chips_row"); // get game's chip number
-    
+    var key = window.event.keyCode;
+    if (key === 13) {
+        // add to the frontend
+        var x = player_name_row.insertCell(-1);
+        var name = new_player_name_input.value;
+        x.innerHTML = name;
+        var y = player_chips_row.insertCell(-1);
+
+        y.innerHTML = game_chips;
+        new_player_name_input.value = ''; // reset the name text input
+
+        // add to the backend
+        let new_player = { "name": name, "chip": parseInt(game_chips) };
+        players.push(new_player);
+        new_player_name_input.focus();
+    }
+}
+function add_player_button() {
     // add to the frontend
     var x = player_name_row.insertCell(-1);
-    var name = document.getElementById("new_player_name_input").value;
+    var name = new_player_name_input.value;
     x.innerHTML = name;
     var y = player_chips_row.insertCell(-1);
-    var game_chips = document.getElementById("game_chips").value;
+
     y.innerHTML = game_chips;
-    document.getElementById("new_player_name_input").value = ''; // reset the name text input
+    new_player_name_input.value = ''; // reset the name text input
 
     // add to the backend
-    let new_player = {"name":name, "chips":parseInt(game_chips)};
+    let new_player = { "name": name, "chip": parseInt(game_chips) };
     players.push(new_player);
+    new_player_name_input.focus();
 }
-
 function remove_player() {
-    player_name_row = document.getElementById("player_name_row"); // get new player name
-    player_chips_row = document.getElementById("player_chips_row"); // get game's chip number
-
     // make sure it does not delete the row labels
     if (player_name_row.cells.length < 2) {
         return;
@@ -36,10 +77,22 @@ function remove_player() {
 }
 
 function start_game() {
-    const new_player_name_form = document.getElementById("new_player_name_form");
-    const game_body = document.getElementById("game_body");
-
     new_player_name_form.style.display = "none"; // hide the add player form
-    game_body.style.display = "block"; // show the game body
+    game_control_button.style.display = "block"; // show the game control button
     is_in_game = true;
+}
+
+function quit_game() {
+    // hide the game control buttons
+    game_control_button.style.display = "none";
+    // hide the game table
+    game_body_section.style.display = "none";
+    // show the player chip page
+    starting_chip_section.style.display = "block";
+    // update game status
+    is_in_game = false;
+}
+
+function restart_game() {
+
 }
